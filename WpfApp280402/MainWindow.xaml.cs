767ec949804efeae0bf6a57820a4fc3bdee313f6
Michael Bakshi <michael.bakshi83@gmail.com>
2021-05-04 19:53:22 +0300

    '

--- WpfApp280402/MainWindow.xaml.cs
+++ WpfApp280402/MainWindow.xaml.cs
@@ -22,30 +22,17 @@ namespace WpfApp280402
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string SizeMessage { get; set; }
+       
         public MainWindow()
         {
 
             InitializeComponent();
 
-            SizeMessage = "Please wait";
+            //URLToSize u=new URLToSize();
+            //DataContext = u;
+            this.DataContext = new URLToSize();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-             var urlTextBox = (TextBox)FindName("UrlTextBox");
-            //string url = "https://www.ynet.co.il/home/0,7340,L-8,00.html";
-            WebRequest webRequest = WebRequest.Create(urlTextBox.Text);
-            WebResponse response = webRequest.GetResponse();
-            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-            {
-                string text = reader.ReadToEnd();
-                var sizeTextBlock = (TextBlock)FindName("SizeTextBlock");
-                sizeTextBlock.Inlines.Add(text.Length.ToString());
-                // text.Length == is the length of the result
-            }
-        }
 
         private void UrlTextBox_MouseEnter(object sender, MouseEventArgs e)
         {
--- WpfApp280402/URLToSize.cs
+++ WpfApp280402/URLToSize.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace WpfApp280402
 {
     class URLToSize: INotifyPropertyChanged
     {
+        private bool buttonOffOn;
+        public bool IsEnabled
+        {
+            get
+            {
+                return buttonOffOn;
+            }
+            set
+            {
+                buttonOffOn = value;
+                OnPropertyRaised("IsEnabled");
+            }
+        }
         private string url;
         public string Url
         {
@@ -19,8 +36,11 @@ namespace WpfApp280402
             set
             {
                 url = value;
+                OnPropertyRaised("Url");
             }
         }
+
+        
 
         private string size;
 
@@ -33,15 +53,53 @@ namespace WpfApp280402
             set
             {
                 size = value;
+                OnPropertyRaised("Size");
             }
         }
+        public DelegateCommand DispatcherButton { get; set; }
+        public DelegateCommand AsyncButton { get; set; }
 
         public URLToSize()
         {
-            url = "Nirav";
-        }
+            DispatcherButton = new DelegateCommand(GetSizeByDispatcher);
+            IsEnabled = true;
+            // Url = "http://";
+            //AsyncButton = new DelegateCommand(GetSizeByDispatcher);
 
+        }
+        //string url = "https://www.ynet.co.il/home/0,7340,L-8,00.html";
+
+        public void GetSizeByDispatcher()
+        {
+           
+                IsEnabled = false;
+                Task.Run(() =>
+                {
+                    WebRequest webRequest = WebRequest.Create(Url);
+                    WebResponse response = webRequest.GetResponse();
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string text = reader.ReadToEnd();
+                        Dispatcher.CurrentDispatcher.Invoke(() => { Size = text.Length.ToString(); });
+                    // Size = text.Length.ToString(); 
+                    // text.Length == is the length of the result
+                }
+                });
+                IsEnabled = true;
+
+        }
         public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void OnPropertyRaised(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+        
+
 
     }
 }
