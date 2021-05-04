3dac0a41599ba9804991f62c71cdf96e89b8cb6c
Michael Bakshi <michael.bakshi83@gmail.com>
2021-05-04 14:27:13 +0300

    p

---
+++ WpfApp280402/URLToSize.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp280402
+{
+    class URLToSize: INotifyPropertyChanged
+    {
+        private string url;
+        public string Url
+        {
+            get
+            {
+                return url;
+            }
+            set
+            {
+                url = value;
+            }
+        }
+
+        private string size;
+
+        public string Size
+        {
+            get
+            {
+                return size ;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+
+        public URLToSize()
+        {
+            url = "Nirav";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
